e77384fe8194d86f29ae58bbf45bd8b339b99796
Seth Rubenstein <sethrubenstein@users.noreply.github.com>
2024-10-24 20:33:52 +0000

    Setting up some additional scaffolding

---
+++ src/example-a/view.js
@@ -0,0 +1,13 @@
+/**
+ * WordPress Dependencies
+ */
+import { store, getContext, getElement, getServerState, getServerContext } from '@wordpress/interactivity';
+
+const { state, actions } = store('wp-dev-blog/example-a', {
+    actions: {
+
+    },
+    callbacks: {
+        
+    }
+})
---
+++ src/example-b/view.js
@@ -0,0 +1,19 @@
+/**
+ * WordPress Dependencies
+ */
+import { store, getContext, getElement, getServerState, getServerContext } from '@wordpress/interactivity';
+
+const { state, actions } = store('wp-dev-blog/example-b', {
+    actions: {
+        performActionAgainstOtherStore: () => {
+            const context = getContext();
+            const { targetNamespace } = context;
+            const targetStore = store(targetNamespace);
+            const { actions: targetActions, state: targetState } = targetStore;
+            targetState.title = 'Hello from example-b';
+        }
+    },
+    callbacks: {
+        
+    }
+})
